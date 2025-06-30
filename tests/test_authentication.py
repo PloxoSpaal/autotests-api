ecddb7b395dc78a3ebcfb1ca78e3d2f8c3736bc2
PlohoSpaal <bulzigan.bz@gmail.com>
2025-06-30 17:24:48 +0300

    Фикстуры Pytest. Переносим создание пользователя в фикстуру

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,9 +1,11 @@
 from http import HTTPStatus
+
+from tests.conftest import UserFixture
 from tools.assertions.schema import validate_json_schema
-from clients.users.public_users_client import get_public_users_client
-from clients.authentication.authentication_client import get_authentication_client
+from clients.users.public_users_client import get_public_users_client, PublicUsersClient
+from clients.authentication.authentication_client import get_authentication_client, AuthenticationClient
 from clients.users.users_schema import CreateUserRequestSchema
-from clients.authentication.authentication_schema import LoginResponseSchema
+from clients.authentication.authentication_schema import LoginResponseSchema, LoginRequestSchema
 from tools.assertions.base import assert_status_code
 from tools.assertions.authentication import assert_login_response
 import pytest
@@ -11,13 +13,12 @@ import pytest
 
 @pytest.mark.regression
 @pytest.mark.authentication
-def test_login():
-    public_user_client = get_public_users_client()
-    auth_client = get_authentication_client()
-    request = CreateUserRequestSchema()
-    public_user_client.create_user_api(request)
-    login_response = auth_client.login_api(request)
-    login_response_body = LoginResponseSchema.model_validate_json(login_response.text)
-    assert_status_code(login_response.status_code, HTTPStatus.OK)
-    assert_login_response(login_response_body)
-    validate_json_schema(login_response.json(), login_response_body.model_json_schema())
+def test_login(
+        authentication_client: AuthenticationClient,
+        function_user: UserFixture):
+    request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+    response = authentication_client.login_api(request)
+    response_body = LoginResponseSchema.model_validate_json(response.text)
+    assert_status_code(response.status_code, HTTPStatus.OK)
+    assert_login_response(response_body)
+    validate_json_schema(response.json(), response_body.model_json_schema())
--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 from tools.assertions.schema import validate_json_schema
-from clients.users.public_users_client import get_public_users_client
+from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from tools.assertions.base import assert_status_code
 from tools.assertions.users import assert_created_user_values
@@ -9,10 +9,9 @@ import pytest
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user():
-    public_user_client = get_public_users_client()
+def test_create_user(public_users_client: PublicUsersClient):
     request = CreateUserRequestSchema()
-    response = public_user_client.create_user_api(request)
+    response = public_users_client.create_user_api(request)
     response_body = CreateUserResponseSchema.model_validate_json(response.text)
     assert_status_code(response.status_code, HTTPStatus.OK)
     assert_created_user_values(response_body, request)
