1e39915372b551dfb8420603c0961159f6bbba6c
PlohoSpaal <bulzigan.bz@gmail.com>
2025-07-12 13:37:39 +0300

    Практикуемся в интеграции покрытия в API тесты. Практическое задание: Работа с инструментом swagger-coverage-tool

--- clients/exercises/exercises_client.py
+++ clients/exercises/exercises_client.py
@@ -4,6 +4,7 @@ from clients.api_client import APIClient
 from clients.private_http_builder import get_private_http_client, AuthenticationUserSchema
 from clients.exercises.exercises_schema import *
 from tools.routes import APIRoutes
+from clients.api_coverage import tracker
 
 
 class ExercisesClient(APIClient):
@@ -12,6 +13,7 @@ class ExercisesClient(APIClient):
     """
 
     @allure.step("Get list of exercises")
+    @tracker.track_coverage_httpx(APIRoutes.EXERCISES)
     def get_exercises_api(self, query: GetExercisesQuerySchema) -> Response:
         """
         Метод получает список заданий для определенного курса.
@@ -22,6 +24,7 @@ class ExercisesClient(APIClient):
         return self.get(APIRoutes.EXERCISES, params=query.model_dump(by_alias=True))
 
     @allure.step("Get exercise by id: {exercise_id:}")
+    @tracker.track_coverage_httpx(f"{APIRoutes.EXERCISES}/{{exercise_id}}")
     def get_exercise_api(self, exercise_id: str) -> Response:
         """
         Метод получает информацию о задании по exercise_id.
@@ -32,6 +35,7 @@ class ExercisesClient(APIClient):
         return self.get(f"{APIRoutes.EXERCISES}/{exercise_id}")
 
     @allure.step("Create exercise")
+    @tracker.track_coverage_httpx(APIRoutes.EXERCISES)
     def create_exercise_api(self, request: CreateExerciseRequestSchema) -> Response:
         """
         Метод создаёт задание.
@@ -43,6 +47,7 @@ class ExercisesClient(APIClient):
         return self.post(APIRoutes.EXERCISES, json=request.model_dump(by_alias=True))
 
     @allure.step("Update exercise by id: {exercise_id:}")
+    @tracker.track_coverage_httpx(f"{APIRoutes.EXERCISES}/{{exercise_id}}")
     def update_exercise_api(self, exercise_id: str, request_body: UpdateExerciseRequestSchema) -> Response:
         """
         Метод обновляет данные задания.
@@ -55,6 +60,7 @@ class ExercisesClient(APIClient):
         return self.patch(f"{APIRoutes.EXERCISES}/{exercise_id}", json=request_body.model_dump(by_alias=True))
 
     @allure.step("Delete exercise by id: {exercise_id:}")
+    @tracker.track_coverage_httpx(f"{APIRoutes.EXERCISES}/{{exercise_id}}")
     def delete_exercise_api(self, exercise_id: str) -> Response:
         """
         Метод удаляет задание.
